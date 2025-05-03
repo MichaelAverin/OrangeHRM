35df9ebc051d2818830ef77e66f7260ac581d024
Michael Averin <mikeaverin@Noutbuk-Michael.local>
2025-05-03 13:47:33 +0700

    Добавлены комментарии в коде тестов

--- OrangeHRM.py
+++ OrangeHRM.py
@@ -58,50 +58,55 @@ DELETE_CONFIRM = ("xpath", "(//div[@class='orangehrm-modal-footer']//button)[2]"
 BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
 driver.get(BASE_URL)
 
+# вход в систему со скрытыми данными
 wait.until(EC.element_to_be_clickable(USERNAME_FIELD)).send_keys(os.environ["LOGIN"])
 wait.until(EC.element_to_be_clickable(PASSWORD_FIELD)).send_keys(os.environ["PASSWORD"])
 wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
 
+# создание нового пользователя
 wait.until(EC.element_to_be_clickable(PIM_BUTTON)).click()
 wait.until(EC.element_to_be_clickable(ADD_BUTTON)).click()
 wait.until(EC.element_to_be_clickable(FIRST_NAME_FIELD)).send_keys("Ivan")
 wait.until(EC.element_to_be_clickable(MIDDLE_NAME_FIELD)).send_keys("Ivanovich")
 wait.until(EC.element_to_be_clickable(LAST_NAME_FIELD)).send_keys("Ivanov")
-
+# генерация нового ID пользователя с удалением предыдущего
 employeeID = wait.until(element_to_be_clickable(ID_FIELD))
 employeeID.send_keys(Keys.COMMAND + "A")
 employeeID.send_keys(Keys.DELETE)
 employeeID.send_keys(f"{random.randint(0000, 9999)}")
-
+# создание у нового пользователя данных для входа в систему
 wait.until(EC.element_to_be_clickable(TOGGLE)).click()
 wait.until(EC.element_to_be_clickable(UN_FIELD)).send_keys(os.environ["NEW_LOGIN"])
 wait.until(EC.element_to_be_clickable(PW_FIELD)).send_keys(os.environ["NEW_PASSWORD"])
 wait.until(EC.element_to_be_clickable(CONFIRM_PW_FIELD)).send_keys(os.environ["NEW_PASSWORD"])
 wait.until(EC.element_to_be_clickable(SAVE_BUTTON)).click()
-
+# проверка исчезающего элемента
 wait.until(EC.visibility_of_element_located(SUCCESS_FIELD))
-
+# выход из системы
 wait.until(EC.element_to_be_clickable(PROFILE_BUTTON)).click()
 wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON)).click()
 
+# вход в систему по данным нового пользователя
 wait.until(EC.element_to_be_clickable(USERNAME_FIELD)).send_keys(os.environ["NEW_LOGIN"])
 wait.until(EC.element_to_be_clickable(PASSWORD_FIELD)).send_keys(os.environ["NEW_PASSWORD"])
 wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
 
+# изменение персональных данных пользователя, используя тестирование исчезающих элементов
 wait.until(EC.element_to_be_clickable(MY_INFO)).click()
 wait.until(EC.element_to_be_clickable(CALENDAR)).click()
 wait.until(EC.element_to_be_clickable(FIRST_APRIL)).click()
 wait.until(EC.element_to_be_clickable(NATIONALITY_SELECT)).click()
 wait.until(EC.element_to_be_clickable(NS_AMERICAN)).click()
 wait.until(EC.element_to_be_clickable(SAVE_BUTTON_2)).click()
-
+# загрузка изображения
 wait.until(EC.element_to_be_clickable(PHOTO_BUTTON)).click()
 wait.until(EC.presence_of_element_located(ADD_PHOTO)).send_keys(f"{os.getcwd()}/PHOTO.jpg")
 wait.until(EC.element_to_be_clickable(SAVE_BUTTON_3)).click()
-
+# выход из системы
 wait.until(EC.element_to_be_clickable(MY_INFO_2)).click()
 wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON_2)).click()
 
+# вход в систему по данным администратора и удаление нового пользователя через поиск в системе и с подтверждением операции
 wait.until(EC.element_to_be_clickable(USERNAME_FIELD)).send_keys(os.environ["LOGIN"])
 wait.until(EC.element_to_be_clickable(PASSWORD_FIELD)).send_keys(os.environ["PASSWORD"])
 wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
